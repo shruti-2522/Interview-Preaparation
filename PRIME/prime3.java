a82c367847f7e0850299356886e14468d132e0c1
shruti amrutkar <shrutiamrutkar177@gmail.com>
2021-07-13 10:36:27 +0530

    Anothe programe logicc to fine prime number

---
+++ PRIME/prime3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.*;
+class prime3
+{
+    public  static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Ente any Number:");
+        int n=s.nextInt();
+
+        if(isPrime(n))
+        {
+            System.out.println("Prime Number"+n);
+        }
+        else
+        {
+            System.out.println("Not prime"+n);
+        }
+    }
+
+
+    static boolean isPrime(int n)
+    {
+        if(n<1)
+        {
+            return false;
+        }
+        else{
+            for(int i=2;i<Math.sqrt(n);i++)
+            {
+                if(n%i==0)
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
