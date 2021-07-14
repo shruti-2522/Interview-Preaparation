ef8ee35cc951dc6d24a1fc84ed346a25fc255c20
shruti amrutkar <shrutiamrutkar177@gmail.com>
2021-07-14 08:22:23 +0530

    Calcculate  Facctorial  using recursion

---
+++ Facctorial'/Fact2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Fact2
+{
+
+
+    static int factorial(int n)
+    {
+
+        if(n==0)
+         return 1;
+         else
+         return n*factorial(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n,fact;
+        Scanner  s=new  Scanner(System.in);
+        System.out.println("Enter Any Number:");
+        n=s.nextInt();
+        fact=factorial(n);
+        System.out.println("Factorial of number="+fact);
+    }
+}
